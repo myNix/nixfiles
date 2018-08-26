2a292d170b47e394e1dbd23a0edf8f65d1eb3e42
mvaude <maxime.vaude@gmail.com>
2018-08-26 17:20:29 +0200

    FEATURE add tmux configuration

--- home.nix
+++ home.nix
@@ -1,7 +1,10 @@
 { pkgs, prefix, ... }:
 
-{
+let
+  my-dotfiles-dir = "./config";
+in {
    home-manager.users.maxter = {
      programs.chromium.enable = true;
+     home.file.".tmux.conf".source = ./config/tmux.conf;
    };
 }
